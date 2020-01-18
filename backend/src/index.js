c5f1ed2116d039fdee6c113bcb3b80f581050a78
edsonpag <edsonsilvapagani@gmail.com>
2020-01-17 21:30:17 -0300

    Backend + Frontend(web) + Frontend(mobile) + WebSocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket'); 
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://edson:edson@projeto01-8krcf.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -15,4 +21,4 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
